82f7b562eb26c2ac4d163bd1459eca9b9cfb257e
spreekaiser <thoralf.barth@gmail.com>
2023-05-22 15:31:32 +0200

    added Form + List component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Form.css";
+
+const Form = () => <div className="Form">Form Component</div>;
+
+export default Form;
---
+++ src/components/List/List.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./List.css";
+
+const List = () => <div className="List">List Component</div>;
+
+export default List;
