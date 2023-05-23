972f944707c115668347b9673b327a828dc4d0a9
spreekaiser <thoralf.barth@gmail.com>
2023-05-23 10:02:10 +0200

    fetch state done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Form from "./components/Form/Form";
 import List from "./components/List/List";
-// import { useState } from "react";
+import { useEffect, useState } from "react";
 import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 
@@ -8,7 +8,30 @@ function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
-  const goodWeather = true;
+  const [goodWeather, setGoodWeather] = useState(false);
+
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const response = await fetch(
+          "https://example-apis.vercel.app/api/weather"
+        );
+        const data = await response.json();
+        console.log("Data: ", data);
+
+        setGoodWeather(data.isGoodWeather);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    // fetchWeather();
+    const intervalID = setInterval(fetchWeather, 3000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  // console.log(goodWeather);
 
   function handleAddActivity(newActivity) {
     setActivities((current) => [...current, newActivity]);
